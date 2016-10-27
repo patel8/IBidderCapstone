ebf14342f68c8aa23c8434fa647b102bc6d20eca
markperl <markperl>
2016-10-27 12:58:45 -0500

    fixing bad build file

---
+++ app/src/main/java/edu/uwm/ibidder/notifications/MyFirebaseInstanceIDService.java
@@ -0,0 +1,10 @@
+package edu.uwm.ibidder.notifications;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Manages unique ID's for the notification service
+ */
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    //TODO
+}
