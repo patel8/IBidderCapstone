0205eaccc09b01c6a0613453b4a59348f7cff4e5
brandonhoward <brndonhoward@gmail.com>
2016-11-01 20:28:33 -0500

    Timeline click listener

--- app/src/main/java/edu/uwm/ibidder/Fragments/all_available_task.java
+++ app/src/main/java/edu/uwm/ibidder/Fragments/all_available_task.java
@@ -1,6 +1,7 @@
 package edu.uwm.ibidder.Fragments;
 
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
@@ -8,6 +9,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +19,10 @@ import java.util.ArrayList;
 
 import edu.uwm.ibidder.Adapters.RecyclerAdapter;
 import edu.uwm.ibidder.DividerItemDecoration;
+import edu.uwm.ibidder.ItemClickSupport;
 import edu.uwm.ibidder.R;
+import edu.uwm.ibidder.TaskActivity;
+import edu.uwm.ibidder.TaskActivityII;
 import edu.uwm.ibidder.dbaccess.TaskAccessor;
 import edu.uwm.ibidder.dbaccess.listeners.TaskCallbackListener;
 import edu.uwm.ibidder.dbaccess.models.TaskModel;
@@ -74,6 +79,17 @@ public class all_available_task extends Fragment {
                     }
                 }, 3000);
                    recyclerAdapter.notifyDataSetChanged();
+            }
+        });
+
+        ItemClickSupport.addTo(recyclerView).setOnItemClickListener(new ItemClickSupport.OnItemClickListener() {
+            @Override
+            public void onItemClicked(RecyclerView rv, int position, View v) {
+                TaskModel tm = list.get(position);
+                Intent intent = new Intent(getContext(), TaskActivityII.class);
+                intent.putExtra("task_id", tm.getTaskId());
+                intent.putExtra("task_status", tm.getStatus().toString());
+                startActivity(intent);
             }
         });
 
