dd991cacc55928caafff50d45b392c0e41322931
markperl <markperl>
2016-11-01 20:27:29 -0500

    helper query method

--- app/src/main/java/edu/uwm/ibidder/dbaccess/ReviewAccessor.java
+++ app/src/main/java/edu/uwm/ibidder/dbaccess/ReviewAccessor.java
@@ -1,6 +1,7 @@
 package edu.uwm.ibidder.dbaccess;
 
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
 
 import edu.uwm.ibidder.dbaccess.models.ReviewModel;
 
@@ -10,13 +11,23 @@ import edu.uwm.ibidder.dbaccess.models.ReviewModel;
 public class ReviewAccessor extends BaseAccessor {
 
     /**
-     * Returns the aggregated review set for a user
+     * Returns the ref to a aggregated review set for a user
      *
      * @param userId The user to get the aggregated review for
-     * @return The user's aggregated review object
+     * @return The user's aggregated review ref
      */
     public DatabaseReference getAggregatedReviewByUserIdRef(String userId) {
         return database.getReference("aggregatedReviews/" + userId);
+    }
+
+    /**
+     * Returns a query for the reviews for a user
+     *
+     * @param userId the user to get the reviews for
+     * @return the user's reviews query
+     */
+    public Query getReviewsByUserIdQuery(String userId) {
+        return database.getReference("reviews").orderByChild("userReviewedId").equalTo(userId);
     }
 
     /**
