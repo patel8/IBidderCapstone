80d9226645c38dbb201db95795744c3407c71493
Sagar <patel8@uwm.edu>
2016-11-15 19:57:30 -0600

    Added Review System after user Completes the Task

--- app/src/main/java/edu/uwm/ibidder/Activities/TaskActivityII.java
+++ app/src/main/java/edu/uwm/ibidder/Activities/TaskActivityII.java
@@ -2,32 +2,49 @@ package edu.uwm.ibidder.Activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.media.Rating;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import edu.uwm.ibidder.Fragments.BidderListFragment;
 import edu.uwm.ibidder.Fragments.TaskFragment;
 import edu.uwm.ibidder.FrontEndSupport;
 import edu.uwm.ibidder.R;
+import edu.uwm.ibidder.dbaccess.BidAccessor;
+import edu.uwm.ibidder.dbaccess.ReviewAccessor;
 import edu.uwm.ibidder.dbaccess.TaskAccessor;
 import edu.uwm.ibidder.dbaccess.TaskCompletedAccessor;
+import edu.uwm.ibidder.dbaccess.TaskWinnerAccessor;
 import edu.uwm.ibidder.dbaccess.UserAccessor;
 import edu.uwm.ibidder.dbaccess.listeners.TaskCallbackListener;
+import edu.uwm.ibidder.dbaccess.listeners.TaskWinnerCallbackListener;
 import edu.uwm.ibidder.dbaccess.listeners.UserCallbackListener;
+import edu.uwm.ibidder.dbaccess.models.ReviewModel;
 import edu.uwm.ibidder.dbaccess.models.TaskModel;
+import edu.uwm.ibidder.dbaccess.models.TaskWinnerModel;
 import edu.uwm.ibidder.dbaccess.models.UserModel;
 public class TaskActivityII extends AppCompatActivity {
 
@@ -67,7 +84,11 @@ public class TaskActivityII extends AppCompatActivity {
                 TaskCompletedAccessor taskCompletedAccessor = new TaskCompletedAccessor();
                 taskCompletedAccessor.markTaskCompleted(taskID);
                 Toast.makeText(TaskActivityII.this, taskID+ "Task is Marked Successfully", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(TaskActivityII.this, ProfileActivity.class));
+                AlertDialog dialog = createReviewDialog(taskID);
+                dialog.show();
+
+
+
             }
         });
         viewPager.setAdapter(new CustomAdapter(getSupportFragmentManager(), getApplicationContext()));
@@ -104,6 +125,62 @@ public class TaskActivityII extends AppCompatActivity {
             }
         });
     }
+    private AlertDialog createReviewDialog(final String TaskId) {
+        final AlertDialog ad = new AlertDialog.Builder(this).create();
+        final LayoutInflater inflater = this.getLayoutInflater();
+        ad.setTitle("Review");
+        View view = inflater.inflate(R.layout.alertdialog_review, null);
+        ad.setView(view);
+        final TextView userName = (TextView) view.findViewById(R.id.textViewUserNameReview);
+        final RatingBar ratingBar = (RatingBar) view.findViewById(R.id.ratingBarReview);
+        final EditText description = (EditText) view.findViewById(R.id.editTextDescriptionReview);
+        final Button submit = (Button) view.findViewById(R.id.buttonSumbitReview);
+
+
+        TaskWinnerAccessor taskWinnerAccessor = new TaskWinnerAccessor();
+        taskWinnerAccessor.GetTaskWinnerByTaskIdOnce(TaskId, new TaskWinnerCallbackListener() {
+            @Override
+            public void dataUpdate(final TaskWinnerModel um) {
+
+                UserAccessor userAccessor = new UserAccessor();
+                userAccessor.getUser(um.getWinnerId(), new UserCallbackListener() {
+                    @Override
+                    public void dataUpdate(final UserModel um) {
+                        userName.setText(um.getFirstName()+" "+um.getLastName());
+                        userName.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                startActivity(new Intent(TaskActivityII.this, UserProfileActivity.class).putExtra("UserID", um.getUserId()));
+                            }
+                        });
+                    }
+                });
+
+                submit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        ReviewAccessor reviewAccessor = new ReviewAccessor();
+                        ReviewModel reviewModel = new ReviewModel();
+                        reviewModel.setReviewScore(ratingBar.getNumStars());
+                        reviewModel.setReviewText(description.getText().toString());
+
+                        reviewModel.setReviewWriterId(um.getTaskOwnerId());
+                        reviewModel.setUserReviewedId(um.getWinnerId());
+
+                        reviewAccessor.createReview(reviewModel);
+                        ad.dismiss();
+
+                        startActivity(new Intent(TaskActivityII.this, ProfileActivity.class));
+
+                    }
+                });
+            }
+        });
+
+
+        return ad;
+    }
+
 
     private class CustomAdapter extends FragmentPagerAdapter {
 
